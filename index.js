a46cac6691e01490d82d87af8bac36cbae93c952
Denny Rizky Ramdani <denny@DESKTOP-LMN9F9I.localdomain>
2022-07-23 17:59:29 +0700

    functional get/update todo data api for frontend

--- index.js
+++ index.js
@@ -1,34 +1,69 @@
 require("dotenv").config({ path: "./config.env" });
 const express = require("express");
-const app = express();
+const recordRoutes = express.Router();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const recordRoutes = express.Router();
-let dataarray = []
 const Firestore = require('@google-cloud/firestore');
+let dbresponsedata
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 const db = new Firestore({
   projectId: 'dreiki-webground',
   keyFilename: '/home/denny/react-test/react-todo-back/dreiki-webground-e48b6eec3bea.json',
 });
 
-async function getdatafirestore() {
-  const snapshot = await db.collection('todo-items').get();
+async function getdatafirestore(username='dennyrizram') {
+  dbresponsedata = {'data':[],'date':''}
+  const snapshot = await db.collection('todo-items').doc(username).collection('data').get();
   snapshot.forEach((doc) => {
-    console.log(doc.id, '=>', doc.data());
-    dataarray.push(doc.data())
+    dbresponsedata.data.push(doc.data())
   });
-  console.log(dataarray)
+  await db.collection('todo-items').doc(username).get().then((d)=>{dbresponsedata.date = d.data()["date"]})
+  console.log(dbresponsedata)
 }
 
-app.use(cors());
-app.use(express.json());
-app.get('/', async (req, res) => {
-  await getdatafirestore()
-  res.send(dataarray)
+async function deletedatafirestore(username='dennyrizram') {
+  const snapshot  = await db.collection('todo-items').doc(username).collection('data').get();
+  const batchSize = snapshot.size
+  console.log(batchSize)
+  if (batchSize === 0) {
+    // When there are no documents left, we are done
+    return;
+  }
+  
+  const batch = db.batch();
+  snapshot.docs.forEach((doc) => {
+    batch.delete(doc.ref);
+  });
+  await batch.commit();
+}
+
+async function updatedatafirestore(username,date,data) {
+  const newdate = {'date':date}
+  await db.collection('todo-items').doc(username).set(newdate)
+  if (data.length > 0){
+    await data.forEach((d) => {
+      db.collection('todo-items').doc(username).collection('data').doc(d.id.toString()).set(d)
+    })
+  }
+}
+
+app.post('/get', async (req, res) => {
+  console.log("NEW GET REQUEST")
+  console.log(req.body.user)
+  await getdatafirestore(req.body.user).catch((d)=>{console.error(d)})
+  res.send(dbresponsedata)
+});
+
+app.post('/update', async (req, res) => {
+  console.log("NEW UPDATE REQUEST")
+  console.log(req.body)
+  await deletedatafirestore(req.body.user).then(()=> updatedatafirestore(req.body.user,req.body.date,req.body.data)).catch((d)=>{console.error(d)})
+  res.send({"status":"Done"})
 });
 
 app.listen(port, () => {
-  // perform a database connection when server starts
   console.log(`Server is running on port: ${port}`);
 });
